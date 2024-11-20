41238dffc85f45abde23ceb8ec9477ddf8cd68b5
Brenda Matias <brendamatias.sobral@gmail.com>
2024-11-20 00:23:45 -0300

    chore: add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       backgroundImage: {
         "custom-gradient": "linear-gradient(168deg, #2A2634 0%, #271A45 100%)",
       },
