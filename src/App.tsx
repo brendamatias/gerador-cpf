dbac89a3be1432c49f5757e98b3b1d78707be476
brenda matias <38896055+brendamatias@users.noreply.github.com>
2024-11-19 23:38:18 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="h-screen flex items-center justify-center text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
